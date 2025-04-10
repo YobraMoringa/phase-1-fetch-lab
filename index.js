a9d6bf0caeedb2794be5429935aafe0672f34afc
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-04-10 05:29:10 +0300

    Added fetch functionality in fetchBooks function

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(booksArray => renderBooks(booksArray))
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
+    // console.log(book)
+    // console.log(book.characters)
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
